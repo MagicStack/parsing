562abdcf17737bb13da3071962d7afddd1aa07ee
Elvis Pranskevichus <elvis@edgedb.com>
2021-08-29 14:05:06 -0700

    Add the ability to build parsing with mypyc
    If called with `PARSING_USE_MYPYC=1` environment variable set,
    `setup.py` will now produce a compiled version of parser table
    generation code, which speeds it up by roughly a factor of two,
    reducing total EdgeDB parser table generation time to about
    13 seconds.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+from setuptools import extension as setuptools_ext
 from setuptools import setup
+from setuptools.command import build_ext as setuptools_build_ext
 
 
 extra = {}
@@ -6,8 +11,70 @@ extra = {}
 f = open("README.rst", "r")
 try:
     extra["long_description"] = f.read()
+    extra["long_description_content_type"] = "text/x-rst"
 finally:
     f.close()
+
+
+USE_MYPYC = False
+MYPY_DEPENDENCY = "mypy>=0.910"
+setup_requires = []
+ext_modules = []
+
+if (
+    os.environ.get("PARSING_USE_MYPYC", None) in {"true", "1", "on"}
+    or "--use-mypyc" in sys.argv
+):
+    setup_requires.append(MYPY_DEPENDENCY)
+    # Fool setuptools into calling build_ext.  The actual list of
+    # extensions would get replaced by mypycify.
+    ext_modules.append(
+        setuptools_ext.Extension("parsing.foo", ["parsing/foo.c"])
+    )
+    USE_MYPYC = True
+
+
+class build_ext(setuptools_build_ext.build_ext):  # type: ignore
+    def finalize_options(self) -> None:
+        # finalize_options() may be called multiple times on the
+        # same command object, so make sure not to override previously
+        # set options.
+        if getattr(self, "_initialized", False):
+            return
+
+        if USE_MYPYC:
+            import pkg_resources
+
+            # Double check Cython presence in case setup_requires
+            # didn't go into effect (most likely because someone
+            # imported Cython before setup_requires injected the
+            # correct egg into sys.path.
+            try:
+                import mypy.version
+                from mypyc.build import mypycify
+            except ImportError:
+                raise RuntimeError(
+                    "please install {} to compile parsing from source".format(
+                        MYPY_DEPENDENCY
+                    )
+                )
+
+            mypy_dep = pkg_resources.Requirement.parse(MYPY_DEPENDENCY)
+            if mypy.version.__version__ not in mypy_dep:
+                raise RuntimeError(
+                    "parsing requires {}, got mypy=={}".format(
+                        MYPY_DEPENDENCY, mypy.version.__version__
+                    )
+                )
+
+            self.distribution.ext_modules = mypycify(
+                [
+                    "parsing/automaton.py",
+                    "parsing/grammar.py",
+                ],
+            )
+
+        super(build_ext, self).finalize_options()
 
 
 setup(
@@ -31,11 +98,14 @@ setup(
     ],
     packages=["parsing", "parsing.tests", "parsing.tests.specs"],
     package_data={"parsing": ["py.typed"]},
+    setup_requires=setup_requires,
+    ext_modules=ext_modules,
     extras_require={
         "test": [
             "flake8",
             MYPY_DEPENDENCY,
         ]
     },
+    cmdclass={"build_ext": build_ext},
     **extra,
 )
